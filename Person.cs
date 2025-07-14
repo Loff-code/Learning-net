31b2b1a34ccffbfa3c280195807f40925a439b22
loff-code <s235483@dtu.dk>
2025-07-14 22:49:31 +0200

    cpp class

---
+++ Person.cs
@@ -0,0 +1,29 @@
+﻿public class Person
+{
+    private string name;
+    private int age;
+    public Person() { }
+    public void setName(string name)
+    {
+        this.name = name;
+    }
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
+
+    public string getName()
+    {
+        return name;
+    }
+    public int getAge()
+    {
+        return age;
+    }
+
+    public void doubleAge()
+    {
+        age *= 2;
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,18 +1,41 @@
-﻿Console.WriteLine("PATH?");
-string path = Console.ReadLine();
-Console.WriteLine("Content?");
-string content = Console.ReadLine();
-Console.WriteLine("Token to look for?");
-string token = Console.ReadLine();
+﻿Person person = new Person();
+int input = 0;
 
-System.IO.File.WriteAllText(path, content);
-string fileContent = System.IO.File.ReadAllText(path);
-
-int cnt = 0;
-foreach (var item in fileContent)
+while (true)
 {
-    if (item.ToString() == token)
-        cnt++;
-}
-Console.WriteLine(cnt);
+    try
+    {
+        System.Console.WriteLine("\n1. set name \n2. set age \n3. get name \n4. get age\n5. double age");
+        input = int.Parse(Console.ReadLine());
+        switch (input)
+        {
+            case 1:
+                person.setName(Console.ReadLine());
+                break;
+            case 2:
+                person.setAge(int.Parse(Console.ReadLine()));
+                break;
+            case 3:
+                System.Console.WriteLine(person.getName());
+                break;
+            case 4:
+                System.Console.WriteLine(person.getAge());
+                break;
+            case 5:
+                int age2remember = person.getAge();
+                person.doubleAge();
+                System.Console.WriteLine("Age was " + age2remember + " and is now " + person.getAge());
+                break;
+            default:
+                break;
+        }
 
+
+    }
+    catch (System.FormatException)
+    {
+        System.Console.WriteLine("Wrong format, try again");
+        throw;
+    }
+
+}
