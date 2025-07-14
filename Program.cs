d5109c80d91d8a4d0efa8ccc67da5b64ec6065d9
loff-code <s235483@dtu.dk>
2025-07-14 21:59:24 +0200

    start proj

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("How many names? ");
+int cnt = int.Parse(Console.ReadLine());
+Console.Write("\n");
+List<string> list = new List<string>();
+for (int i = 0; i < cnt; i++)
+{
+    Console.Write("Enter Name: ");
+    list.Add(Console.ReadLine());
+}
+
+foreach (var item in list)
+{
+Console.WriteLine("Hello, " + item + "!");
+}
